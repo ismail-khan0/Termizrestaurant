a153b8a555ca0e705689430239079dfb0cd3b32d
ismail-khan0 <ik8323253@gmail.com>
2025-11-27 00:28:04 -0800

    Fix React version and add proper configuration

---
+++ electron/main.js
@@ -0,0 +1,53 @@
+﻿const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+    icon: path.join(__dirname, "../public/icon.png"),
+    show: false,
+  });
+
+  const startUrl = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  mainWindow.loadURL(startUrl);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+    if (isDev) {
+      mainWindow.webContents.openDevTools();
+    }
+  });
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ electron/preload.js
@@ -0,0 +1,5 @@
+﻿const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  // You can add IPC methods here if needed
+});
